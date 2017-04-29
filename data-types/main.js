f7472d4b5118c2ac422120425b753dcd9d9e1789
Alumno09 S304 <a09s304@campustls.local>
2017-04-29 14:01:05 -0500

    pusheo trabajo de sabado

---
+++ data-types/main.js
@@ -0,0 +1,19 @@
+var isBoolean = true;
+var isString= "bruno";
+var isNumber= 19;
+var isObjetc= {
+	name: "goku",
+	lastName: "z",
+}
+var isArray= ["bruno", "liu", "chavez"];
+
+var isFunction = function() {
+	return "hola soy una funcion"
+}
+
+	 console.log(isBoolean, typeof isBoolean);
+	 console.log(isString, typeof isString);
+	 console.log(isNumber, typeof isNumber);
+	 console.log(isObjetc, typeof isObjetc);
+	 console.log(isFunction, typeof isFunction);
+	 console.log(isArray, typeof isArray);
---
+++ flow-control/main.js
@@ -0,0 +1,34 @@
+/*var studentName = "bruno";
+
+switch( typeof studentName) {
+	case "number": 
+		console.log("es un numero");
+		break;
+	case  "booleano":
+		 console.log("es un boolean");
+		 break;
+	case "string" :
+		console.log("es un string");
+		break;
+	default:
+		console.log("no tengo idea");
+	} */
+
+
+
+/*try {
+	var numb1 = 10;
+
+	console.log(numb1/numb2);
+} catch (e){
+
+}*/
+
+function suma(a, b){
+	console.log(a + b);
+}
+
+function ejecutaFuncion(a, b, callback){
+	callback(a, b);
+
+}
